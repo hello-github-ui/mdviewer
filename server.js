3c4fbb9dd7d16ca9a713a980e1185fd6d04e6b1d
hello-github-ui <util.you.come@gmail.com>
2024-07-11 18:06:10 +0800

    修改ico以及404处理

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const __dirname = path.dirname(__filename);
 // 设置静态文件目录
 frontendApp.use(express.static(path.join(__dirname, './docs')));
 
+// 404 处理
+frontendApp.get("/*",(req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, './404.html'));
+});
+
 // 启动静态文件服务
 frontendApp.listen(frontendPort, () => {
     console.log(`Static file server running on port http://localhost:${frontEndPort}`);
