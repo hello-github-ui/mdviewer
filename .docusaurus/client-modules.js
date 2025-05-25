2663f872c55628bccbf30bd57b202785291515f6
hello-github-ui <util.you.come@gmail.com>
2025-05-25 16:27:50 +0800

    应用重构-搜索预计切换为algolia

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\code\\my-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\code\\my-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\code\\my-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\code\\my-docs\\src\\css\\custom.css'),
-];
